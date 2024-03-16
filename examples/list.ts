52dcf319b3af7d395121e1a17bfc7e02700da645
artfuldev <hello@artful.dev>
2024-03-16 22:00:30 +0000

    Illustrate using named function arguments

--- examples/list.ts
+++ examples/list.ts
@@ -1,12 +1,12 @@
-import { generic, recursive, union, has } from "../src";
+import { generic, recursive, union, of } from "../src";
 
 const List = union("List")(
   recursive((ListA) =>
     generic((A) => ({
-      Empty: has(),
-      Cons: has(A, ListA),
+      Empty: of<[]>(),
+      Cons: of<[head: typeof A, tail: typeof ListA]>(),
     }))
   )
 );
 const empty = List.Empty<number>();
-const singleton = List.Cons(42, List.Empty());
+const singleton = List.Cons(42, empty);
