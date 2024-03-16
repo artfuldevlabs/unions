bada7bcea1b26264d571cbff4f9fa9be78b81596
artfuldev <hello@artful.dev>
2024-03-16 21:51:39 +0000

    Remove unnecessary functions

--- src/tagged.ts
+++ src/tagged.ts
@@ -2,20 +2,3 @@ export type Tagged<Tag extends string, Args extends any[] = []> = {
   tag: Tag;
   args: Args;
 };
-
-export const tagged =
-  <Args extends any[] = []>(...args: Args) =>
-  <Tag extends string>(tag: Tag): Tagged<Tag, Args> => ({
-    tag,
-    args,
-  });
-
-export type TagConstructor<Args extends any[], Tag extends string> = (
-  ...args: Args
-) => Tagged<Tag, Args>;
-
-export const tag =
-  <Args extends any[] = []>() =>
-  <Tag extends string>(tag: Tag): TagConstructor<Args, Tag> =>
-  (...args) =>
-    tagged(...args)(tag);
